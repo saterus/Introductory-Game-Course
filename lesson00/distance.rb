d27b09f1a69bb308da97134524fc6eaee18f587b
Alex Burkhart <saterus@gmail.com>
2011-03-11 20:38:11 -0500

    Created first lesson.

---
+++ lesson00/distance.rb
@@ -0,0 +1,33 @@
+
+# Write your method definitions here:
+########################################
+
+# reminder of how to define methods
+def foo(file_name)
+  puts "Successfully loaded #{file_name}."
+end
+
+# Part 1
+
+# Part 2
+
+# Part 3
+
+# Part 4
+
+# Part 5
+
+########################################
+
+# stuff inside this "if" gets executed when we run this file.
+if __FILE__ == $0
+
+  # test out your methods here
+  ########################################
+
+  # reminder of how to call methods
+  foo(__FILE__)
+
+  ########################################
+
+end
